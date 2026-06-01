37d8a760ecca69fa791a680cde9f9a8ef8ee0b9a
Mitra <sanghamitra912@gmail.com>
2026-06-01 11:53:55 +1000

    Fix Nuxt dev app manifest resolution

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
   ssr: true,
   devtools: { enabled: false },
+  experimental: {
+    appManifest: false
+  },
   css: ['~/assets/css/main.css'],
   app: {
     head: {
